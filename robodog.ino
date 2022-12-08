65a7996e1a4117be9ed0cb7b15685d98a1f7270f
Ian Lansdowne <idlansdowne@gmail.com>
2022-12-08 16:15:52 -0600

    2 part foot path

--- robodog.ino
+++ robodog.ino
@@ -26,9 +26,9 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // WiFiServer server(80);
 
 // servos 7 and 10 will need extra calibration
-#define SERVO_TESTING 11
+// #define SERVO_TESTING 4
 leg right_front((servo_t){9, 70, 442, M_PI_2, -M_PI_2}, (servo_t){10, 76, 436, M_PI_2, -M_PI_2}, (servo_t){11, 84, 436, M_PI, 0}, &pwm, UPPER_LEN_M, LOWER_LEN_M, FOOT_RADIUS_M),
-    left_front((servo_t){6, 68, 432, M_PI_2, -M_PI_2}, (servo_t){7, 68, 428, -M_PI_2, M_PI_2}, (servo_t){8, 70, 420, 0, M_PI}, &pwm, UPPER_LEN_M, LOWER_LEN_M, FOOT_RADIUS_M),
+    left_front((servo_t){6, 68, 432, M_PI_2, -M_PI_2}, (servo_t){7, 68, 428, -M_PI_2, M_PI_2}, (servo_t){8, 80, 420, 0, M_PI}, &pwm, UPPER_LEN_M, LOWER_LEN_M, FOOT_RADIUS_M),
     right_back((servo_t){3, 72, 440, -M_PI_2, M_PI_2}, (servo_t){4, 60, 416, M_PI_2, -M_PI_2}, (servo_t){5, 48, 404, M_PI, 0}, &pwm, UPPER_LEN_M, LOWER_LEN_M, FOOT_RADIUS_M),
     left_back((servo_t){0, 52, 412, -M_PI_2, M_PI_2}, (servo_t){1, 58, 422, -M_PI_2, M_PI_2}, (servo_t){2, 60, 420, 0, M_PI}, &pwm, UPPER_LEN_M, LOWER_LEN_M, FOOT_RADIUS_M);
 
@@ -159,69 +159,21 @@ void runGyro(void *args)
 
 void runPWM(void *args)
 {
-  // while (1)
-  // {
-  //   while (!Serial.available())
-  //     delay(10);
-  //   pwm.setPWM(SERVO_TESTING, 0, Serial.parseInt());
-  //   // for (float i = .03; i < .1; i += .001)
-  //   // {
-  //   //   left_back.move_to(0, 0, i);
-  //   //   delay(30);
-  //   // }
-  // }
+#ifdef SERVO_TESTING
+  while (1)
+  {
+    while (!Serial.available())
+      delay(10);
+    pwm.setPWM(SERVO_TESTING, 0, Serial.parseInt());
+  }
+#endif
   // pwm.setPWM(1, 0, RAD_TO_SERVO_MID(0));
   // pwm.setPWM(2, 0, RAD_TO_SERVO(M_PI));
-
-  // while (1)
-  // {
-  //   right_front.move_to(0, 0, .1);
-  //   left_back.move_to(0, 0, .1);
-  //   left_front.move_to(0, 0, .1);
-  //   right_back.move_to(0, 0, .1);
-
-  //   delay(1000);
-
-  //   right_front.move_to(.04, 0, .1);
-  //   left_back.move_to(.04, 0, .1);
-  //   left_front.move_to(.04, 0, .1);
-  //   right_back.move_to(.04, 0, .1);
-
-  //   delay(1000);
-
-  //   right_front.move_to(-.04, 0, .1);
-  //   left_back.move_to(-.04, 0, .1);
-  //   left_front.move_to(-.04, 0, .1);
-  //   right_back.move_to(-.04, 0, .1);
-
-  //   delay(1000);
-
-  //   right_front.move_to(0, 0, .1);
-  //   left_back.move_to(0, 0, .1);
-  //   left_front.move_to(0, 0, .1);
-  //   right_back.move_to(0, 0, .1);
-
-  //   delay(1000);
-
-  //   right_front.move_to(0, 0, .06);
-  //   left_back.move_to(0, 0, .06);
-  //   left_front.move_to(0, 0, .06);
-  //   right_back.move_to(0, 0, .06);
-
-  //   delay(1000);
-
-  //   right_front.move_to(0, 0, .13);
-  //   left_back.move_to(0, 0, .13);
-  //   left_front.move_to(0, 0, .13);
-  //   right_back.move_to(0, 0, .13);
-
-  //   delay(1000);
-  // }
 
   unsigned long start_time, t, temp;
   float x, z;
 
-  const unsigned cycle_time = 300;
+  const unsigned cycle_time = 400U;
   unsigned phase = 0;
   const float path_radius = .02f;
 
@@ -232,61 +184,28 @@ void runPWM(void *args)
   {
     for (leg cur_leg : legs)
     {
-      temp = (t + phase) % cycle_time;
-      if (temp <= cycle_time / 2)
+      temp = t + phase;
+      if (temp % cycle_time <= cycle_time / 2)
       {
-        x = -path_radius * cos((float)temp / cycle_time * M_PI);
-        z = .08f - path_radius * sin((float)temp / cycle_time * M_PI);
+        temp %= cycle_time / 2;
+        x = -path_radius * cos(2.0f * temp / cycle_time * M_PI);
+        z = .12f - path_radius * 0.5f * sin(2.0f * temp / cycle_time * M_PI);
       }
       else
       {
-        temp = temp % (cycle_time / 2);
-        x = path_radius - 2.0f * path_radius / cycle_time * temp;
-        z = .08f;
+        temp %= cycle_time / 2;
+        x = path_radius - 4.0f * path_radius / cycle_time * temp;
+        z = .12f;
       }
+      if (phase % (cycle_time / 2))
+        x -= .02f;
+      else
+        x += .02f;
       cur_leg.move_to(x, 0, z);
       phase += cycle_time / 4;
     }
     phase = 0;
     t = millis() - start_time;
-    delay(5);
+    delay(2);
   }
-
-  vTaskDelete(pwmTask);
-
-  // int t = 0;
-  // float x = t - sin(t), z = 1 - cos(t);
-  for (float i = .12; i >= .03; i -= 0.001)
-  {
-    // float x = 0.0, z = i;
-    // float d2 = x * x + z * z; // Distance b/w circles squared (for efficiency)
-    // float c1 = UPPER_LEN_M * UPPER_LEN_M - LOWER_LEN_M * LOWER_LEN_M + d2;
-    // float c2 = c1 / (2.0 * d2);
-    // if (4 * UPPER_LEN_M * UPPER_LEN_M >= c1 * c1 / d2) // Is there a solution? To optimize later
-    // {
-    //   float c3 = sqrtf(4 * UPPER_LEN_M * UPPER_LEN_M - c1 * c1 / d2) / (2 * sqrtf(d2));
-    //   float servo1 = -atanf((c2 * z + c3 * x) / (c2 * x - c3 * z));
-    //   float servo2 = M_PI - acos((UPPER_LEN_M * UPPER_LEN_M + LOWER_LEN_M * LOWER_LEN_M - d2) / (2.0 * UPPER_LEN_M * LOWER_LEN_M));
-    //   Serial.print(i);
-    //   Serial.print(" ");
-    //   Serial.print(c2 * z + c3 * x);
-    //   Serial.print(" ");
-    //   Serial.print(c2 * x - c3 * z);
-    //   Serial.print(" ");
-    //   Serial.print(servo1);
-    //   Serial.print(" ");
-    //   Serial.println(servo2);
-    //   pwm.setPWM(0, 0, RAD_TO_SERVO_MID(-servo1));
-    //   pwm.setPWM(1, 0, RAD_TO_SERVO(servo2));
-    // }
-    // else
-    // {
-    //   pwm.setPWM(0, 0, RAD_TO_SERVO_MID(0));
-    //   pwm.setPWM(1, 0, RAD_TO_SERVO(0));
-    // }
-    // // pwm.setPWM(0, 0, RAD_TO_SERVO(M_PI_4));
-    // // pwm.setPWM(1, 0, RAD_TO_SERVO(M_PI_4));
-    // delay(30);
-  }
-  vTaskDelete(pwmTask);
 }
